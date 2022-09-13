817ae75a677a12be1967152764bec552889b823e
Jackie Liu <jackil@microsoft.com>
2022-09-13 09:46:18 -0700

    Impl last two commands for finishing PAL3A core gameplay
    Impl last two commands for finishing PAL3A core gameplay

---
+++ Assets/Scripts/Pal3/Command/SceCommands/UnknownCommand182.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------------------------------------
+//  Copyright (c) 2021-2022, Jiaqi Liu. All rights reserved.
+//  See LICENSE file in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+namespace Pal3.Command.SceCommands
+{
+    #if PAL3A
+    [SceCommand(182, "???")]
+    public class UnknownCommand182 : ICommand
+    {
+        public UnknownCommand182(
+            string unknown)
+        {
+            Unknown = unknown;
+        }
+
+        public string Unknown { get; }
+    }
+    #endif
+}
---
+++ Assets/Scripts/Pal3/Command/SceCommands/UnknownCommand194.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------------------
+//  Copyright (c) 2021-2022, Jiaqi Liu. All rights reserved.
+//  See LICENSE file in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+namespace Pal3.Command.SceCommands
+{
+    #if PAL3A
+    [SceCommand(194, "PAL3游戏结束的最后一个指令，目测是播放制作人信息???")]
+    public class UnknownCommand194 : ICommand
+    {
+        public UnknownCommand194() { }
+    }
+    #endif
+}
