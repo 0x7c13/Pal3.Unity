49f25b255ed2547896307f30d142b646e0655c25
Jackie Liu <jackil@microsoft.com>
2022-10-26 00:07:05 -0700

    Add ToonMaterialFactory, temp fix for normals

---
+++ Assets/Scripts/Pal3/Renderer/MaterialFactoryBase.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------------------------------------
+//  Copyright (c) 2021-2022, Jiaqi Liu. All rights reserved.
+//  See LICENSE file in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+namespace Pal3.Renderer
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public abstract class MaterialFactoryBase
+    {
+        private const string SPRITE_SHADER_PATH = "Pal3/Sprite";
+        
+        // Sprite material uniforms
+        private static readonly int SpriteMainTexPropertyId = Shader.PropertyToID("_MainTex");
+        
+        private readonly Dictionary<string, Shader> _shaders = new ();
+        
+        public Material CreateSpriteMaterial()
+        {
+            return new Material(GetShader(SPRITE_SHADER_PATH));
+        }
+        
+        public Material CreateSpriteMaterial(Texture2D texture)
+        {
+            var material = new Material(GetShader(SPRITE_SHADER_PATH));
+            material.SetTexture(SpriteMainTexPropertyId, texture);
+            return material;
+        }
+        
+        internal Shader GetShader(string shaderName)
+        {
+            if (_shaders.ContainsKey(shaderName)) return _shaders[shaderName];
+
+            var shader = Shader.Find(shaderName);
+            
+            if (shader != null)
+            {
+                _shaders[shaderName] = shader;
+            }
+            
+            return shader;
+        }
+    }
+}
---
+++ Assets/Scripts/Pal3/Renderer/ToonMaterialFactory.cs
@@ -0,0 +1,80 @@
+﻿// ---------------------------------------------------------------------------------------------
+//  Copyright (c) 2021-2022, Jiaqi Liu. All rights reserved.
+//  See LICENSE file in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+namespace Pal3.Renderer
+{
+    using System.Collections.Generic;
+    using Core.GameBox;
+    using UnityEngine;
+
+    public class ToonMaterialFactory : MaterialFactoryBase, IMaterialFactory
+    {
+        // Toon material uniforms
+        private static readonly int CutoutPropertyId = Shader.PropertyToID("_Cutout");
+        
+        private static readonly Dictionary<string, Shader> Shaders = new ();
+
+        private readonly Material _toonMaterial;
+        private readonly Shader _toonShader;
+        
+        public ToonMaterialFactory()
+        {
+            _toonMaterial = Resources.Load<Material>("Materials/Toon");
+            _toonShader = _toonMaterial.shader;
+        }
+
+        /// <inheritdoc/>
+        public Material CreateWaterMaterial(Texture2D mainTexture, 
+            Texture2D shadowTexture, 
+            float alpha)
+        {
+            Material material = new Material(_toonShader);
+            material.CopyPropertiesFromMaterial(_toonMaterial);
+            material.mainTexture = mainTexture;
+            return material;
+        }
+
+        /// <inheritdoc/>
+        public Material[] CreateStandardMaterials(
+            Texture2D mainTexture,
+            Texture2D shadowTexture,
+            Color tintColor,
+            GameBoxBlendFlag blendFlag,
+            float transparentThreshold)
+        {
+            Material[] materials = null;
+
+            if (blendFlag is GameBoxBlendFlag.AlphaBlend or GameBoxBlendFlag.InvertColorBlend)
+            {
+                materials = new Material[1];
+                materials[0] = CreateTransparentMaterial(mainTexture);
+            }
+            else if (blendFlag == GameBoxBlendFlag.Opaque)
+            {
+                materials = new Material[1];
+                materials[0] = CreateOpaqueMaterial(mainTexture);
+            }
+            
+            return materials;
+        }
+        
+        private Material CreateTransparentMaterial(Texture2D mainTexture)
+        {
+            Material material = new Material(_toonShader);
+            material.CopyPropertiesFromMaterial(_toonMaterial);
+            material.SetFloat(CutoutPropertyId, 0.3f);
+            material.mainTexture = mainTexture;
+            return material;
+        }
+        
+        private Material CreateOpaqueMaterial(Texture2D mainTexture)
+        {
+            Material material = new Material(_toonShader);
+            material.CopyPropertiesFromMaterial(_toonMaterial);
+            material.mainTexture = mainTexture;
+            return material;
+        }
+    }
+}
