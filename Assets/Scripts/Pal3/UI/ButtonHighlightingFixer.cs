6c7eecd150afde0787f1e7383997c9d044b7a6a8
Jackie Liu <jackil@microsoft.com>
2023-03-21 18:42:54 -0700

    Fix button highlighting issue when mouse and controller/keyboard are used at same time

---
+++ Assets/Scripts/Pal3/UI/ButtonHighlightingFixer.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------------------------------------
+//  Copyright (c) 2021-2023, Jiaqi Liu. All rights reserved.
+//  See LICENSE file in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+namespace Pal3.UI
+{
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    // Taken from https://forum.unity.com/threads/button-keyboard-and-mouse-highlighting.294147/
+    [RequireComponent(typeof(Selectable))]
+    public class ButtonHighlightingFixer : MonoBehaviour, IPointerEnterHandler, IDeselectHandler, IPointerExitHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!EventSystem.current.alreadySelecting)
+            {
+                EventSystem.current.SetSelectedGameObject(gameObject);
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (EventSystem.current.currentSelectedGameObject == gameObject &&
+                !EventSystem.current.alreadySelecting)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            GetComponent<Selectable>().OnPointerExit(null);
+        }
+    }
+}
