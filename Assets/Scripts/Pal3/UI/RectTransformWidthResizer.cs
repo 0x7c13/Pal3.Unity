4d23ec3c47af2643f14620b18eb055d56381c86c
Jackie Liu <jackil@microsoft.com>
2023-06-29 11:23:27 -0700

    v0.64

---
+++ Assets/Scripts/Pal3/UI/RectTransformWidthResizer.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------------------------------------
+//  Copyright (c) 2021-2023, Jiaqi Liu. All rights reserved.
+//  See LICENSE file in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+namespace Pal3.UI
+{
+    using UnityEngine;
+
+    [ExecuteAlways]
+    [RequireComponent(typeof(RectTransform))]
+    public class RectTransformWidthResizer : MonoBehaviour
+    {
+        public float thresholdRatio;
+        public float minWidth;
+        public float maxWidth;
+
+        private RectTransform _transform;
+        private Canvas _canvas;
+
+        void OnEnable()
+        {
+            _transform = GetComponent<RectTransform>();
+        }
+
+        void Update()
+        {
+            bool useMinWidth = (float)Screen.width / Screen.height < thresholdRatio;
+            _transform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, useMinWidth ? minWidth : maxWidth);
+        }
+
+        void OnValidate() => OnEnable();
+    }
+}
