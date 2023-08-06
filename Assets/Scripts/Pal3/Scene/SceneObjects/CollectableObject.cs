b0eca975891ba537f1935d0d79c289117de561ab
Jiaqi Liu <jackil@microsoft.com>
2023-08-06 09:44:45 -0700

    Tweaks and fixes

--- Assets/Scripts/Pal3/Scene/SceneObjects/CollectableObject.cs
+++ Assets/Scripts/Pal3/Scene/SceneObjects/CollectableObject.cs
@@ -14,7 +14,7 @@ namespace Pal3.Scene.SceneObjects
     [ScnSceneObject(ScnSceneObjectType.Collectable)]
     public sealed class CollectableObject : SceneObject
     {
-        private const float MAX_INTERACTION_DISTANCE = 3f;
+        private const float MAX_INTERACTION_DISTANCE = 3.5f;
 
         public CollectableObject(ScnObjectInfo objectInfo, ScnSceneInfo sceneInfo)
             : base(objectInfo, sceneInfo)
